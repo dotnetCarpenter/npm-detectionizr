e6fa512a49c2edce2a523c1d89b83212a286cb9f
dotnetCarpenter <jon.ronnenberg@gmail.com>
2013-02-13 20:17:02 +0100

    began work on better tests

--- test/detectionizr_test.js
+++ test/detectionizr_test.js
@@ -1,5 +1,3 @@
-var detectionizr = require('../lib/detect.js');
-
 /*
   ======== A Handy Little Nodeunit Reference ========
   https://github.com/caolan/nodeunit
@@ -20,69 +18,138 @@ var detectionizr = require('../lib/detect.js');
     test.ifError(value)
 */
 
-module.exports = {
+var unitTests = {
   setUp: function(done) {
-    this.commands = {
-      0: ["imagemagick", "command", "punycode", "rdjpgcom", "imagemagick", "imgcheck"],
-      1: ["cluster", "os", "http", "punycode"],
-      2: ["which", "command", "whereis"]
-    }
+    //TODO: need to implement process signaling in detectionizr, before it can be tested rigorously
+    // or stop using a singleton pattern
+    //this.detectionizr = require("child_process").fork("../lib/detect.js"/*, [args], [options]*/);
+    this.detectionizr = require("../lib/detect.js");
+    this.public = [
+      "child_process",
+      "require",
+      "detect",
+      "on",
+      "overwrite"
+    ];
+    done();
+  },
+  tearDown: function(done) {
+    // TODO: See todo in setUp
+    //this.detectionizr.kill("SIGTERM");
+    this.detectionizr = null; // doesn't do much
     done();
   },
   api: function(test) {
     test.expect(5);
-    test.ok(detectionizr.child_process)
-    test.ok(detectionizr.require)
-    test.ok(detectionizr.detect)
-    test.ok(detectionizr.on)
-    test.ok(detectionizr.overwrite)
+    test.ok(this.detectionizr.child_process)
+    test.ok(this.detectionizr.require)
+    test.ok(this.detectionizr.detect)
+    test.ok(this.detectionizr.on)
+    test.ok(this.detectionizr.overwrite)
     test.done();
   },
-  detect: function(test) {
-    test.expect(3);
-    var fn = function(){};
-    var expectedObject = {
-      0: {
-        child_process: fn,
-        require:fn,
-        detect:fn,
-        on:fn,
-        overwrite:fn,
-        imagemagick:false,
-        command:fn,
-        punycode:fn,
-        rdjpgcom:true,
-        imgcheck:false
-      },
-      1: {
-        child_process: fn,
-        require:fn,
-        detect:fn,
-        on:fn,
-        overwrite:fn,
-        cluster:fn,
-        os:fn,
-        http:fn,
-        punycode:fn
-      },
-      2: {
-        child_process: fn,
-        require:fn,
-        detect:fn,
-        on:fn,
-        overwrite:fn,
-        which:fn,
-        command:fn,
-        whereis:fn
-      }
-    }
-    for (var detectSerie in this.commands) {
-      test.notDeepEqual(
-        detectionizr.detect(this.commands[detectSerie])
-      , expectedObject[detectSerie]
-      , this.commands[detectSerie].toString()
+  nodeModules: function(test) {
+    var commands = [
+      'assert',
+      'buffer',
+      /*'child_process', is already included in detectionizr */
+      'cluster',
+      'crypto',
+      'dgram',
+      'dns',
+      'events',
+      'fs',
+      'http',
+      'https',
+      'net',
+      'os',
+      'path',
+      'punycode',
+      'querystring',
+      'readline',
+      'repl',
+      'string_decoder',
+      'tls',
+      'tty',
+      'url',
+      'util',
+      'vm',
+      'zlib' ];  // node 0.8.0 internal modules
+
+    var propertyList = commands.concat(this.public);
+
+    test.expect(propertyList.length);
+    
+    this.detectionizr.detect(commands);
+    for (var prop in this.detectionizr) {
+      test.ok(
+          propertyList.indexOf(prop) > -1
+        , "Expected " + prop
       );
-    }    
+    }
+    //console.dir(this.detectionizr);
     test.done();
+  },/*
+  dependencies: function(test) {
+    var commands = ["grunt"];   // npm package used to run these tests
+    var propertyList = commands.concat(this.public);
+    test.expect(propertyList.length);
+    this.detectionizr.detect(commands);
+    for (var prop in this.detectionizr) {
+      //console.log(prop)
+      test.ok(
+          propertyList.indexOf(prop) > -1
+        , "Expected " + prop
+      );
+    }
+    test.done();      
+  },/*
+  systemCommands: function(test) {
+    var commands = ["which", "command"] // commands used to find system libs
+    test.expect(commands.length);
+    
+    var propertyList = commands.concat(this.public);
+    this.detectionizr.detect(commands);
+    for (var prop in this.detectionizr) {
+      //console.log(prop)
+      test.ok(
+          propertyList.indexOf(prop) > -1
+        , "Expected " + prop
+      );
+    }
+    test.done();
+  },*/
+  detect: function(test) {
+    var d = this.detectionizr.detect(['grunt', 'imagemagick']);
+    test.ok(!d.imagemagick);
+    test.ok(d.grunt);
+    test.done();
+  },
+  on: function(test) {
+    var d = this.detectionizr;
+    var tests = ["g++","cpp","ar", "ranlib", "exiv2", "c++"];
+    var numTests = tests.length;
+    test.expect(numTests);
+    d.on("detect", function log(libname, exist) {
+        numTests--;
+        test.equal(numTests, numTests)  // count test.expect
+        //TODO: need to implement process signaling
+        //test.ok(tests.indexOf(libname) > -1, libname)
+        if(numTests === 0)
+            test.done();
+    });
+    d.detect(tests)
   }
 };
+
+module.exports = unitTests;
+
+/*function propertyCheck(actual, expected) {
+  for(var prop in actual) {
+    console.log(prop)
+    if(!expected[prop]) {
+      return false;
+    }
+  }
+  return true;
+}*/
