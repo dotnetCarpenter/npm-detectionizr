d1d518d7f6ffc61f3e7643382f67fe0c93e6e2ce
dotnetCarpenter <jon.ronnenberg@gmail.com>
2013-02-13 07:10:53 +0100

    more manual tests - really need to do proper unit testing

--- test/testDetect.js
+++ test/testDetect.js
@@ -1,6 +1,12 @@
-/* MANUAL TEST
-
+/* MANUAL TEST */
+/*
 var test = require("../lib/detect");
+// be sure to attach event handler before you use detect
+test.on("detect", function(name, exist) {
+  if(name === "ls" && test.ls)
+    console.log("We can use test.ls");
+});
+
 test.detect(["imagemagick", "ls", "punycode"]);
 
 if(test.punycode)
@@ -8,45 +14,51 @@ if(test.punycode)
 if(test.imagemagick)
 	console.log("We can use test.imagemagick");
 
-test.on("detect", function(name, exist) {
-	if(test.ls)
-		console.log("We can use test.ls");
-});
-
-console.dir(test)*/
+*/
+/*
 var test = require("../lib/detect");
 function PackageManager() {
     this.available = [];
 }
 PackageManager.prototype.recieve = function(name, exist) {
     if(exist) this.available.push(name)
-    console.dir(pm);    // { available: [ 'rdjpgcom' ] }
-    console.dir(test);  /* { child_process: 
-                               { fork: [Function],
-                                 _forkChild: [Function],
-                                 exec: [Function],
-                                 execFile: [Function],
-                                 spawn: [Function] },
-                              require: [Function: r],
-                              detect: [Function],
-                              on: [Function],
-                              overwrite: [Function],
-                              rdjpgcom: true, <- command line libraries can not be referenced
-                              imagemagick: <- but nodejs modules are referenced
-                               { identify: { [Function] path: 'identify' },
-                                 readMetadata: [Function],
-                                 convert: { [Function] path: 'convert' },
-                                 resize: [Function],
-                                 crop: [Function],
-                                 resizeArgs: [Function] },
-                              imgcheck: false }*/
 }
 var pm = new PackageManager();
 test.on("detect", pm.recieve, pm);
+test.on("finally", function() {
+  console.dir(pm);    // { available: [ 'imagemagick', 'rdjpgcom' ] }
+  console.dir(test);  /* { child_process: 
+                           { fork: [Function],
+                             _forkChild: [Function],
+                             exec: [Function],
+                             execFile: [Function],
+                             spawn: [Function] },
+                          require: [Function: r],
+                          detect: [Function],
+                          on: [Function],
+                          overwrite: [Function],
+                          rdjpgcom: true, <- command line libraries can not be referenced
+                          imagemagick: <- but nodejs modules are referenced
+                           { identify: { [Function] path: 'identify' },
+                             readMetadata: [Function],
+                             convert: { [Function] path: 'convert' },
+                             resize: [Function],
+                             crop: [Function],
+                             resizeArgs: [Function] },
+                          imgcheck: false }*/
+/*
+});
 test.detect(["rdjpgcom", "imagemagick", "imgcheck"]);
-
-
-/*var test = require("../lib/detect");
+*/
+/*
+var test = require("../lib/detect");
 test.detect(["cluster", "os", "http", "punycode"]);
 console.dir(test)
+*/
+/*var d = require('../lib/detect').detect(['grunt', 'imagemagick'])
+d.imagemagick ?                   
+  console.log("imagemagick is here") :
+    d['grunt'] ?
+      console.log("grunt is here") :
+      console.error("No grunt and imagemagick") 
 */
